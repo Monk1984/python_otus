41a53fc44a681f1d811ee60a528feddbdfae7f56
Monk1984 <ionov-alexei84@mail.ru>
2022-02-05 19:09:25 +0300

    New otus

--- trening.py
+++ trening.py
@@ -1,15 +1,16 @@
+from os.path import exists
+
 
 b = 20
 c = b/2.54
-print('Равен в дюймах %s.' %c)
+print('Равен в дюймах %s.' % c)
 
-from os.path import exists
 
-#Как код ниже разместить в одну строку?
+# Как код ниже разместить в одну строку?
 in_file = open('C:/Users/Aleksey/Desktop/from_file.txt')
 indata = in_file.read()
 
-print( "Исходный файл имеет размер %s" % len(indata))
+print("Исходный файл имеет размер %s" % len(indata))
 print("Файл назначения существует? %r" % exists(indata))
 
 out_input = open('C:/Users/Aleksey/Desktop/to_file.txt', 'w')
